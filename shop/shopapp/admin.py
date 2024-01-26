1754cc42f75a2ac3f1577096d54b6cbe0ba021bb
Marya373 <z89262554967@gmail.com>
2024-01-26 18:03:22 +0300

    filter

--- shop/shopapp/admin.py
+++ shop/shopapp/admin.py
@@ -13,8 +13,13 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter=['date_time_additions_product']
     search_fields =['name']
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'date_time_placing_order']
+    list_filter=['client', 'date_time_placing_order']
+    search_fields =['client']
+
 admin.site.register(Client)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 
 
