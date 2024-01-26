6dfb495b94917c9a1492f095c34118a6ab36f2bd
Marya373 <z89262554967@gmail.com>
2024-01-26 16:58:06 +0300

    register_admin

--- shop/shopapp/admin.py
+++ shop/shopapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from shopapp.models import Client, Product, Order
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
+
+
